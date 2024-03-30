b544c6261c899e314bf812bb2b4ccf79cf12d0c4
Suchit Gupta <suchitgupta027@gmail.com>
2024-03-30 13:39:14 +0530

    check

--- Lab/lab11/q2.py
+++ Lab/lab11/q2.py
@@ -1,39 +1,38 @@
-def genlist(mat):
+def genlist(queens):
     adList = []
-    for i in range(8):
-        node = [[0, 0, 0, 0, 0, 0, 0, 0] for x in range(8)]
-        for x in range(7):
-            for y in range(8):
-                if mat[x][i] == 1:
-                    node[x + 1][i] = 1
-                else:
-                    node[x][y] = mat[x][y]
-        adList.append(node)
+    for i in range(4):
+        node = [x for x in queens]
+        if node[i] < 3:
+            node[i] += 1
+        mat = [[0] * 4 for x in range(4)]
+        for x in range(4):
+            mat[node[x]][x] = 1
+        adList.append((mat, node))
     return adList
 
 
 def test(mat):
     count = 0
-    for i in range(8):
+    for i in range(4):
         row = mat[i]
         for x in row:
             if x == 1:
                 count += 1
-        if count > 1:
-            return False
+            if count > 1:
+                return False
         count = 0
 
-    for i in range(8):
-        for j in range(8):
+    for i in range(4):
+        for j in range(4):
             if mat[j][i] == 1:
                 count += 1
-        if count > 1:
-            return False
+            if count > 1:
+                return False
         count = 0
-    for i in range(8):
-        for j in range(8):
+    for i in range(4):
+        for j in range(4):
             if mat[i][j] == 1:
-                for x, y in zip(range(i, -1, -1), range(j, -1, -1)):
+                for x, y in zip(range(i, 4), range(j, 4)):
                     if mat[x][y] == 1:
                         return False
     return True
@@ -41,18 +40,25 @@ def test(mat):
 
 def bfs():
     count = 0
-    node = [[0, 0, 0, 0, 0, 0, 0, 0] for x in range(8)]
-    for i in range(8):
+    node = [[0, 0, 0, 0] for x in range(4)]
+    for i in range(4):
         node[0][i] = 1
-    queue = [node]
+    queue = [(node, [0, 0, 0, 0])]
+    visit = []
     while queue:
         queue = queue[::-1]
         curr = queue.pop()
+        visit.append(curr[1])
         queue = queue[::-1]
-        if test(curr):
+        if curr[1] == [2, 0, 3, 1]:
+            print(curr[1])
+        if test(curr[0]):
             count += 1
-        adList = genlist(curr)
+            print(count)
+        adList = genlist(curr[1])
         for x in adList:
+            if x[1] in visit:
+                continue
             queue.append(x)
     print(count)
 
